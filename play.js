f44c7c0dd49bd2e0b0c93630239b5350ec29d0fb
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-12-11 16:20:21 +0000

    enhancement: add post and get single route

---
+++ play.js
@@ -0,0 +1,9 @@
+const myObj = {
+  myName: "michael",
+  myFavFood: {
+    food1: "beans",
+    food2: "apple",
+  },
+};
+
+console.log(myObj.myFavFood.food2);
--- src/server.js
+++ src/server.js
@@ -4,10 +4,44 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/book", (request, response) => {
-  response.send({ title: "LotR", author: "tolkein" });
+const books = [];
+
+app.post("/book", (request, response) => {
+  books.push(request.body);
+
+  const successResponse = {
+    message: "book added",
+    books: books,
+  };
+
+  response.send(successResponse);
 });
+
+app.get("/book", (request, response) => {
+  const index = books.findIndex((x) => x.title === request.body.title);
+
+  const successResponse = {
+    message: "book found",
+    book: books[index],
+  };
+
+  response.send(successResponse);
+});
+
+app.delete("/book");
+
+app.put("/book");
 
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
+
+// if (HTTP === "GET") {
+//   // do some get stuff
+// } else if (HTTP === "POST") {
+//   // do some post stuff
+// }
+
+// const response = await fetch("http://localhost:5001/book", {
+//     method: "DELETE"
+// });
