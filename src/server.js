beb66b403d9114727bbc8d52660b1bc45761bb6b
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-12-11 14:29:41 +0000

    chore: removed static server/files

--- src/server.js
+++ src/server.js
@@ -2,10 +2,6 @@ const express = require("express");
 
 const app = express();
 
-// http://localhost:5001/davelikescars
-app.use("/example", express.static("example"));
-app.use("/pizza", express.static("motorbikes"));
-
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
