5e619099eeb3beecec33f3cc392878c5ad656cd3
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2023-12-11 14:44:27 +0000

    enhancement: add a get route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/book", (request, response) => {
+  response.send({ title: "LotR", author: "tolkein" });
+});
+
 app.listen(5001, () => {
   console.log("Server is listening on port 5001");
 });
